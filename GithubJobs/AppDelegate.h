dc07016930971c95a7af0f6dbf73c5df836e1ac4
mishingena <mishingena@gmail.com>
2018-06-04 12:22:42 +0300

    Initial Commit

---
+++ GithubJobs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GithubJobs
+//
+//  Created by Gena on 04.06.2018.
+//  Copyright © 2018 GM Groups. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GithubJobs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithubJobs
+//
+//  Created by Gena on 04.06.2018.
+//  Copyright © 2018 GM Groups. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
